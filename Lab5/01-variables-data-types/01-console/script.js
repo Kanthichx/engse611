ce0a936b295da51d642b6dfb15c5839d0e0aeb50
Kanthichx <itchxm@gmail.com>
2025-03-03 13:02:52 +0700

    add all-file

---
+++ Lab5/01-variables-data-types/01-console/script.js
@@ -0,0 +1,93 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let a=1;
+let b=2;
+let c=1.3;
+
+console. log (a, typeof a);
+console. log (b, typeof b);
+console. log (c, typeof c);
+let bigNumber=10000000n;
+console. log(bigNumber, typeof bigNumber );
+
+console. log(cats, typeof cats);
+console. log(my)
+
+// convert st
+//amonut = pa
+//console. log
+
+
+
+//NaN -> Not-
+
+//amonut = +a
+amonut = Number(amonut);
+console. log(amonut, typeof  amonut);
+console. log(parentInt("---------"));
+console. log(parentInt("34"));
+console. log(parentInt("-56"));
+console. log(parentIntarseInt("3.14"));
+console. log(parentInt("6.022e23"));
+
+console. log(Namber("---------"));
+console. log(Namber("34"));
+console. log(Namber("-56"));
+console. log(Namber("3.14"));
+console. log(Namber("6.022e23"));
+
+console .log('-------');
+amonut = 1024;
+console .log(amount, typeof amonut);
+
+/*
+//convert namber to string
+amonut = amonut.tostring();
+amonut. log(amonut, typeof amonut);
+amonut= parent (amonut);
+console .log(amonut, typeof amonut);
+amonut = string
+   
+console. log(1+2*2-3)
+//(3);
+console. log(6 == 0);
+
+//and
+0 && 0 =0
+0 && 1 =0
+1 && 0 =0
+1 && 1 =1
+
+//or
+0 || 0 =
+0 || 1 =
+1 || 0 =
+1 || 1 =
+
+//not
+! 0 = 1
+! 1 =0
+
+if ((1 == '1') && ( 3 == '3') && (4 == '4'))
+
+
+
+
